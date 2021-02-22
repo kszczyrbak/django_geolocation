ef04ab1f04d14f38122731fa28b662be27f34308
Chris <kszczyrbak8@gmail.com>
2021-02-22 14:01:04 +0100

    changed token obtain api url

--- geolocations/urls.py
+++ geolocations/urls.py
@@ -13,7 +13,7 @@ router.register(r'locations', GeolocationViewset, basename='locations')
 
 
 urlpatterns = [
-    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/signup', RegisterView.as_view()),
 ]
